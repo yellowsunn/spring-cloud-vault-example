cba228ca68b654a8d97d545e8b4e98f22fa37e73
yellowsunn <chohankook95@gmail.com>
2023-10-02 02:07:32 +0900

    init project

---
+++ src/main/kotlin/com/yellowsunn/springcloudvaultexample/SpringCloudVaultExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.yellowsunn.springcloudvaultexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCloudVaultExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringCloudVaultExampleApplication>(*args)
+}
